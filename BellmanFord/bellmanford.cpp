d05815888b5336f1615cf6ff01ce9cd98961e0b9
Vo Thach Chi Truong <vtchitruong@gmail.com>
2024-06-30 19:40:31 +0700

    Add new cpp file. Modify some input and output files.

--- BellmanFord/bellmanford.cpp
+++ BellmanFord/bellmanford.cpp
@@ -3,154 +3,163 @@
 #include <fstream>
 #include <stack>
 #include <map>
+#include <iomanip>
 
-#define inputFile "bf.inp"
-#define outpuFile "bf.out"
+#define inputFile "bf3.inp"
+#define outpuFile "bf3.out"
 
 using namespace std;
 
-int vNum, eNum, start, finish; // number of vertices, number of edges
+int numberOfVertices, numberOfEdges, start, finish;
 vector<vector<int>> edges;
 
-vector<int> d; // distance
+vector<int> d;
 vector<int> trace;
 const int INF = 2E9; // infinity
-bool negativeCycle = false; // the graph has some negative cycle or not
+bool negativeCycle = false;
 
-//-----------------------------------------------------------------------------
 void Input()
 {
     ifstream f;
     f.open(inputFile);
 
-    f >> vNum >> eNum >> start >> finish;
+    f >> numberOfVertices >> numberOfEdges >> start >> finish;
 
-    // read edges
-    for (int i = 0; i < eNum; ++i)
+    for (int i = 0; i < numberOfEdges; ++i)
     {
-        int u, v, len; // length
-        f >> u >> v >> len;
-        vector<int> edge{u, v, len};
+        int u, v, w; // weight
+        f >> u >> v >> w;
+        vector<int> edge{u, v, w};
         edges.push_back(edge);
     }
 
     f.close();
 }
 
-//-----------------------------------------------------------------------------
-void Init()
-{
-    // d[v] is the minimum distance from start to v
-    d.resize(vNum + 1, INF);
-    d[start] = 0;
-
-    // trace[v] = u means that u is followed by v, or we have to travel from u to v
-    trace.resize(vNum + 1);
-}
-
-//-----------------------------------------------------------------------------
 void Show_d()
 {
-    for (int i = 0; i < d.size(); ++i)
+    for (int i = 1; i < d.size(); ++i)
     {
-        cout << d[i] << ' ';
+        cout << "(" << d[i] << ", " << trace[i] << ") |";
     }
     cout << endl;
 }
 
-//-----------------------------------------------------------------------------
 void BellmanFord()
 {
-    Init();
+    // Bước 0:
+    // Khởi tạo d[v] là khoảng cách ngắn nhất từ start đến v
+    d.resize(numberOfVertices + 1, INF);
+    d[start] = 0;
 
-    for (int k = 1; k < vNum; ++k) // k <= vNum - 1
+    // Khởi tạo trace[v] = u, nghĩa là trước đỉnh v là đỉnh u
+    trace.resize(numberOfVertices + 1, 0);
+
+    // Bước 1:
+    // Lặp (v - 1) lần cập nhật khoảng cách (relaxation)
+    for (int i = 1; i <= numberOfVertices - 1; ++i)
     {
-        for (int i = 0; i < edges.size(); ++i)
+        for (int j = 0; j < numberOfEdges; ++j)
         {
-            int u = edges[i][0];
-            int v = edges[i][1];
-            int weight = edges[i][2] ; // weight of u -> v
+            int u = edges[j][0];
+            int v = edges[j][1];
+            int weight = edges[j][2] ;
 
+            // Xét xem có đường đi từ start đến u hay không
             if (d[u] != INF)
+            {
+                // Xét đường đi từ start đến v mà thông qua u liệu có thể ngắn hơn được hay không
                 if (d[u] + weight < d[v])
                 {
                     d[v] = d[u] + weight;
                     trace[v] = u;
                 }
+            }
         }
+
+        Show_d();
     }
 
-    cout << "Array d after iterating k times" << endl;
-    Show_d();
+    
 
-    for (int i = 0; i < edges.size(); ++i)
+    // Bước 2: Phát hiện chu trình mang trọng số âm
+    for (int i = 0; i < numberOfEdges; ++i)
     {
         int u = edges[i][0];
         int v = edges[i][1];
-        int weight = edges[i][2] ; // weight of u -> v
+        int weight = edges[i][2] ;
 
         if (d[u] != INF)
+        {
+            // Nếu weight là âm thì khoảng cách d sẽ bị giảm
+            // Suy ra có chu trình âm
             if (d[u] + weight < d[v])
-            {
-                d[v] = d[u] + weight;                
-                negativeCycle = true; // the graph has some negative cycle
+            {          
+                negativeCycle = true;
+                d[v] = d[u] + weight;
+                trace[v] = u;
+                
+                return;
             }
+        }
     }
 
-    cout << "Array d after iterating 1 more times" << endl;
     Show_d();
 }
 
-//-----------------------------------------------------------------------------
 void Output()
 {
-    stack<int> p; // path
+    stack<int> path;
 
-    if (d[finish] == INF) // cannot reach finish
+    if (d[finish] == INF)
     {
-        p.push(-1);
+        path.push(-1);
     }
     else if (negativeCycle)
     {
-        p.push(-2);
+        path.push(-2);
     }
     else
     {
-        int f = finish; // temporary finish
+        int fn = finish; // biến tạm
 
-        // trace back from finish to start for output path
-        while (f != start)
+        // Dựa vào mảng trace, cho fn "lùi" dần về start
+        while (fn != start)
         {
-            p.push(f);
-            f = trace[f];
+            path.push(fn);
+            fn = trace[fn];
         }
 
-        p.push(start);
+        path.push(start);
     }
     
     ofstream f;
     f.open(outpuFile);
 
-    f << d[finish] << endl;
-    while (!p.empty())
+    // Nếu không phải giá trị -1 hoặc -2
+    if (path.top() >= 0)
     {
-        f << p.top();
-        if (p.size() != 1) f << ' ';
-        p.pop();
+        // thì output đường đi ngắn nhất
+        f << d[finish] << endl;
+    
+        while (!path.empty())
+        {
+            f << path.top();
+            if (path.size() != 1) f << ' ';
+            path.pop();
+        }
     }
-
+    else
+    {
+        f << path.top();
+    }   
+    
     f.close();
 }
 
-//-----------------------------------------------------------------------------
 int main()
 {
     Input();
-    for (auto i = edges.begin(); i != edges.end(); ++i)
-    {
-        cout << (*i)[0] << ' ' << (*i)[1] << ' ' << (*i)[2] << endl;
-    }
-
     BellmanFord();
     Output();
 
