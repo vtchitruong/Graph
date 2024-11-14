bc77dae3622439b17ea1d4b61a088608d5565693
Vo Thach Chi Truong <vtchitruong@gmail.com>
2024-11-14 13:07:57 +0700

    Modify some code and comment

--- RobotMove/robot_move.py
+++ RobotMove/robot_move.py
@@ -1,117 +1,162 @@
 import os
 import sys
-from collections import deque
+import queue
 
-input_file = os.path.join(sys.path[0], 'robotmove2.inp')
-output_file = os.path.join(sys.path[0], 'robotmove2.out')
+input_file = os.path.join(sys.path[0], 'robotmove.inp')
+output_file = os.path.join(sys.path[0], 'robotmove.out')
 
-#-----------------------------------------------------------------------------
-def input():
-    with open(input_file) as f:
-        global rows, cols, start, finish        
+row_number = 0
+col_number = 0
+maze = []
 
-        # first line in input file
-        rows, cols, s_row, s_col, f_row, f_col = map(int, f.readline().split())
+# Ô xuất phát và ô kết thúc
+start = []
+finish = []
+
+# Mảng trace dùng để truy vết đường đi
+trace = []
+
+
+def input_data():
+    global row_number, col_number, maze, start, finish
+
+    with open(input_file, 'r') as f:
+        row_number, col_number, s_row, s_col, f_row, f_col = map(int, f.readline().split())
         start = [s_row, s_col]
         finish = [f_row, f_col]
 
         # read the remnaining lines for the maze
         lines = f.readlines()
-        global maze
-        maze = list()
-        maze.append([]) # for line 0
-        for line in lines:                        
-            tmp = [int(c) for c in line if c != '\n' ]
-            tmp.insert(0, 0) # for column 0
-            maze.append(tmp)
 
-#-----------------------------------------------------------------------------
+        # Hàng có chỉ số 0
+        maze.append([])
+
+        for line in lines:
+            line = '0' +  line.strip()            
+            maze.append(list(map(int, line)))
+
+
 def init():
     global trace
-    trace = [['N' for col in range(cols + 1)] for row in range(rows + 1)] # N: not visited
+    
+    # Khởi tạo giá trị 'N' cho toàn bộ mảng trace
+    trace = [['N' for col in range(col_number + 1)] for row in range(row_number + 1)]
+    
+    # Ô xuất phát được đánh dấu bằng ký tự 'S'
     trace[start[0]][start[1]] = 'S'
 
-#-----------------------------------------------------------------------------
+
 def bfs():
-    q = deque()
-    q.append(start)
+    # Hàng đợi chứa các ô cần duyệt
+    q = queue.Queue()
 
+    # Nạp ô xuất phát vào hàng đợi
+    q.put(start)
+
+    # Trong khi hàng đợi vẫn còn ô chờ duyệt
     while q:
-        current = q[0]
+        # Lấy ô ở đầu hàng đợi
+        current = q.get()
 
-        # return if the robot reaches the finish cell
+        # Dừng vòng lặp khi ô sắp duyệt là ô kết thúc, tức đã đến đích
         if current == finish:
             return
 
-        # try 4 directions in turn
+        # Thử đi lên một ô
         if current[0] > 1: # if robot could go up
-            if trace[current[0] - 1][current[1]] == 'N':
-                if maze[current[0] - 1][current[1]] == 0:
+            if maze[current[0] - 1][current[1]] == 0:
+                if trace[current[0] - 1][current[1]] == 'N':
+                    # Lấy toạ độ của ô trên
                     next = current.copy()
                     next[0] -= 1
-                    q.append(next)
+
+                    # Đẩy vào hàng đợi
+                    q.put(next)
+
+                    # Đánh dấu đã duyệt bằng ký tự U: up
                     trace[next[0]][next[1]] = 'U'
 
-        if current[0] < rows: # if robot could go down
-            if trace[current[0] + 1][current[1]] == 'N':
-                if maze[current[0] + 1][current[1]] == 0:
+        # Thử đi xuống một ô
+        if current[0] < row_number:
+            if maze[current[0] + 1][current[1]] == 0:
+                if trace[current[0] + 1][current[1]] == 'N':
+                    # Lấy toạ độ của ô dưới
                     next = current.copy()
                     next[0] += 1
-                    q.append(next)
+
+                    # Đẩy vào hàng đợi
+                    q.put(next)
+
+                    # Đánh dấu đã duyệt bằng ký tự D: down
                     trace[next[0]][next[1]] = 'D'
 
-        if current[1] > 1: # if robot could go left
-            if trace[current[0]][current[1] - 1] == 'N':
-                if maze[current[0]][current[1] - 1] == 0:
+        # Thử qua trái một ô
+        if current[1] > 1:
+            if maze[current[0]][current[1] - 1] == 0:
+                if trace[current[0]][current[1] - 1] == 'N':
+                    # Lấy toạ độ của ô trái
                     next = current.copy()
                     next[1] -= 1
-                    q.append(next)
+
+                    # Đẩy vào hàng đợi
+                    q.put(next)
+
+                    # Đánh dấu đã duyệt bằng ký tự L: left
                     trace[next[0]][next[1]] = 'L'
 
-        if current[1] < cols: # if robot could go right
-            if trace[current[0]][current[1] + 1] == 'N':
-                if maze[current[0]][current[1] + 1] == 0:
+        # Thử qua phải một ô
+        if current[1] < col_number:
+            if maze[current[0]][current[1] + 1] == 0:
+                if trace[current[0]][current[1] + 1] == 'N':
+                    # Lấy toạ độ của ô phải
                     next = current.copy()
                     next[1] += 1
-                    q.append(next)
-                    trace[next[0]][next[1]] = 'R'
-        
-        q.popleft() # remove current
 
-#-----------------------------------------------------------------------------
-def output():   
+                    # Đẩy vào hàng đợi
+                    q.put(next)
+
+                    # Đánh dấu đã duyệt bằng ký tự R: right
+                    trace[next[0]][next[1]] = 'R'
+
+
+def process():
+    init();
+    bfs();
+
+
+def output():
+    # Đường đi kết quả
     path = list()
 
     if trace[finish[0]][finish[1]] != 'N':
+        # Dựa vào mảng trace để truy vết các ô nằm trước ô kết thúc
         while not trace[finish[0]][finish[1]] == 'S':
-            tmp = finish.copy()
-            path.append(tmp)
-            t = trace[finish[0]][finish[1]]
-            if t == 'U':
+            # Đẩy ô kết thúc vào ngăn xếp path
+            path.append(finish.copy())
+
+            # Tiếp tục lùi ô kết thúc đến vị trí mới
+            direction= trace[finish[0]][finish[1]]
+            if direction== 'U':
                 finish[0] += 1
-            elif t == 'D':
+            elif direction== 'D':
                 finish[0] -= 1
-            elif t == 'L':
+            elif direction== 'L':
                 finish[1] += 1
-            elif t == 'R':
+            elif direction== 'R':
                 finish[1] -= 1
         
+        # Đẩy ô xuất phát vào ngăn xếp
         path.append(start)
     
     with open(output_file, 'w') as f:
         if not path:
-            f.write(-1)
+            f.write('-1')
         else:
-            for i in range(len(path) - 1, 0, -1):
-                cell = f'{path[i][0]} {path[i][1]}'
-                print(cell, file=f)
-            
-            cell = f'{path[0][0]} {path[0][1]}'
-            f.write(cell)
+            f.writelines([f'{path[i][0]} {path[i][1]}\n' for i in range(len(path) - 1, 0, -1)])
+            f.write(f'{path[0][0]} {path[0][1]}')
 
-#-----------------------------------------------------------------------------
-if __name__ == '__main__':    
-    input()    
-    init()
-    bfs()
+
+if __name__ == '__main__':
+    input_data()
+    process()
     output()
--- RobotMove/robotmove.cpp
+++ RobotMove/robotmove.cpp
@@ -1,133 +1,169 @@
 #include <iostream>
-#include <fstream>
 #include <vector>
 #include <queue>
 #include <stack>
 
-#define inputFile "robotmove2.inp"
-#define outputFile "robotmove2.out"
+#define input_file "robotmove.inp"
+#define output_file "robotmove.out"
 
 using namespace std;
 
-int rows, cols;
-pair<int,int> start, finish; // 1-based index
-vector<vector<short>> maze;
+int row_number, col_number;
+vector<vector<char>> maze;
 
+// Ô xuất phát và ô kết thúc
+pair<int,int> start, finish;
+
+// Mảng trace dùng để truy vết đường đi
 vector<vector<char>> trace;
 
-void Input()
-{
-    ifstream f;
-    f.open(inputFile);
 
-    // load data to global variables
-    f >> rows >> cols >> start.first >> start.second >> finish.first >> finish.second;
+void input()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    freopen(input_file, "r", stdin);
+
+    cin >> row_number >> col_number >> start.first >> start.second >> finish.first >> finish.second;
     
-    // read each row, then add them to vector g
-    string eachRow;
-    maze.resize(rows + 1);
-    for (int r = 1; r < rows + 1; ++r)
-    {
-        maze[r].resize(cols + 1);
-        
-        f >> eachRow;
-        for (int c = 1; c < cols + 1; ++c)
+    // Khởi tạo ma trận maze gồm toàn phần tử 0
+    maze.resize(row_number + 1, vector<char>(col_number + 1, 0));
+
+    // Đọc từng hàng của input và đưa vào mảng maze
+    string row;
+    for (int r = 1; r < row_number + 1; ++r)
+    {        
+        cin >> row;
+
+        for (int c = 1; c < col_number + 1; ++c)
         {
-            int val = eachRow[c - 1] == '0' ? 0 : 1;
-            maze[r][c] = val;
+            if (row[c - 1] == '1') maze[r][c] = 1;
         }        
     }
-    
-    f.close();
 }
 
-void Init()
+
+void init()
 {
-    trace.resize(rows + 1);
+    // Khởi tạo giá trị 'N' cho toàn bộ mảng trace
+    trace.resize(row_number + 1, vector<char>(col_number + 1, 'N'));
 
-    for (int r = 1; r < rows + 1; ++r)
-    {
-        trace[r].resize(cols + 1, 'N'); // 'N' = not yet visited
-    }
-
+    // Ô xuất phát được đánh dấu bằng ký tự 'S'
     trace[start.first][start.second] = 'S';
 }
 
-void Bfs()
+
+void bfs()
 {
+    // Hàng đợi chứa các ô cần duyệt
     queue<pair<int, int>> q;
+
+    // Nạp ô xuất phát vào hàng đợi
     q.push(start);
     
-    pair<int, int> current; // current cell of the robot
-    pair<int, int> next; // temp cell for next move
+    pair<int, int> current;
+    pair<int, int> next;
 
+    // Trong khi hàng đợi vẫn còn ô chờ duyệt
     while (!q.empty())
     {
+        // Lấy ô ở đầu hàng đợi
         current = q.front();
-        
-        // return if the robot reaches the finish cell
+        q.pop();
+
+        // Dừng vòng lặp khi ô sắp duyệt là ô kết thúc, tức đã đến đích
         if (current == finish)
             return;
 
-        // try 4 directions in turn
-        if (current.first > 1) // robot could go up
-            if (trace[current.first - 1][current.second] == 'N')
-                if (maze[current.first - 1][current.second] == 0)
+        // Thử đi lên một ô
+        if (current.first > 1)
+            if (maze[current.first - 1][current.second] == 0)
+                if (trace[current.first - 1][current.second] == 'N')
                 {
+                    // Lấy toạ độ của ô trên
                     next = current;
                     next.first--;
 
+                    // Đẩy vào hàng đợi
                     q.push(next);
+
+                    // Đánh dấu đã duyệt bằng ký tự U: up
                     trace[next.first][next.second] = 'U';
                 }
         
-        if (current.first < rows) // robot could go down
-            if (trace[current.first + 1][current.second] == 'N')
-                if (maze[current.first + 1][current.second] == 0)
+        // Thử đi xuống một ô
+        if (current.first < row_number)
+            if (maze[current.first + 1][current.second] == 0)
+                if (trace[current.first + 1][current.second] == 'N')
                 {
+                    // Lấy toạ độ của ô dưới
                     next = current;
                     next.first++;
 
+                    // Đẩy vào hàng đợi
                     q.push(next);
+
+                    // Đánh dấu đã duyệt bằng ký tự D: down
                     trace[next.first][next.second] = 'D';
                 }
 
-        if (current.second > 1) // robot could go left
-            if (trace[current.first][current.second - 1] == 'N')
-                if (maze[current.first][current.second - 1] == 0)
+        // Thử qua trái một ô
+        if (current.second > 1)
+            if (maze[current.first][current.second - 1] == 0)
+                if (trace[current.first][current.second - 1] == 'N')
                 {
+                    // Lấy toạ độ của ô trái
                     next = current;
                     next.second--;
 
+                    // Đẩy vào hàng đợi
                     q.push(next);
+
+                    // Đánh dấu đã duyệt bằng ký tự L: left
                     trace[next.first][next.second] = 'L';
                 }
         
-        if (current.second < cols) // robot could go right
-            if (trace[current.first][current.second + 1] == 'N')
-                if (maze[current.first][current.second + 1] == 0)
+        // Thử qua phải một ô
+        if (current.second < col_number)
+            if (maze[current.first][current.second + 1] == 0)
+                if (trace[current.first][current.second + 1] == 'N')
                 {
+                    // Lấy toạ độ của ô phải
                     next = current;
                     next.second++;
 
+                    // Đẩy vào hàng đợi
                     q.push(next);
+
+                    // Đánh dấu đã duyệt bằng ký tự R: right
                     trace[next.first][next.second] = 'R';
                 }
-
-        q.pop();
-    }    
+    }
 }
 
-void Output()
+
+void process()
 {
+    init();
+    bfs();
+}
+
+
+void output()
+{
+    // Đường đi kết quả
     stack<pair<int, int>> path;
 
     if (trace[finish.first][finish.second] != 'N')
     {
+        // Dựa vào mảng trace để truy vết các ô nằm trước ô kết thúc
         while (trace[finish.first][finish.second] != 'S')
         {
+            // Đẩy ô kết thúc vào ngăn xếp path
             path.push(finish);
 
+            // Tiếp tục lùi ô kết thúc đến vị trí mới
             switch (trace[finish.first][finish.second])
             {
                 case 'U': finish.first++;
@@ -140,35 +176,35 @@ void Output()
                     break;
             }
         }
+
+        // Đẩy ô xuất phát vào ngăn xếp
         path.push(start);
     }
 
-    ofstream f;
-    f.open(outputFile);
+    freopen(output_file, "w", stdout);
 
     if (path.empty())
     {
-        f << -1;
+        cout << -1;
     }
     else
     {
         while (path.size() > 1)
         {
-            f << path.top().first << ' ' << path.top().second << endl;
+            cout << path.top().first << ' ' << path.top().second << '\n';
             path.pop();
         }
-        f << path.top().first << ' ' << path.top().second;
-    }
 
-    f.close();
+        cout << path.top().first << ' ' << path.top().second;
+    }
 }
+
 
 int main()
 {
-    Input();
-    Init();  
-    Bfs();
-    Output();
+    input();
+    process();
+    output();
     
     return 0;
 }
